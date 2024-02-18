2e510123b5200ca63f70e9e13781006cd27946cf
Siddhant Maitra <smaitra.dev@gmail.com>
2024-02-19 02:49:13 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import RecipeList from './RecipeList';
+// import Button from './Button'; // A default import
+import {PlayButton,UploadButton} from './Button';
+import Greeting from './Greeting';
+import SculptGallery from './SculptGallery';
+import Counter from './Counter';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
+      <h2> React is hard </h2>
+      <RecipeList className="left" />
+      <div className='breakers'> <h2>Componentization trials</h2></div>
+      <PlayButton movieName={"JJK"}/>
+      <UploadButton/>
+      <div className='breakers'> <h2>Learning useState</h2></div>
+      <Greeting/>
+      <div className='breakers'></div>
+      <SculptGallery/>
+      <div className='breakers'>Counter</div>
+      <Counter/>
+
+
     </div>
   );
 }
